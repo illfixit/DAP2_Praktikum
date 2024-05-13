6534980442e6749299e247b94ec0eb79bd254a7c
illfixit <66363651+illfixit@users.noreply.github.com>
2024-05-13 22:43:28 +0200

    add: B5A2

--- Blatt_5/B5A2.java
+++ Blatt_5/B5A2.java
@@ -20,6 +20,21 @@ class B5A2 {
         // Ersetzen Sie diese Kommentarzeile durch Ihren Code!
 
         /**********************************************************/
+        if (m1 > m2 || p1 < p2)
+            return false;
+
+        for (int i = l; i < m1; i++) {
+            if (data[i] < p1)
+                return false;
+        }
+        for (int i = m1 + 1; i < m2; i++) {
+            if (data[i] < p2 || data[i] > p1)
+                return false;
+        }
+        for (int i = m2 + 1; i <= r; i++) {
+            if (data[i] > p2)
+                return false;
+        }
         return true;
 
     }
@@ -27,17 +42,31 @@ class B5A2 {
     // l,r inclusive -> partition data[l,r]
     public static int[] partition(int[] data, int l, int r) {
 
-        /**********************************************************/
-        /**** Kurzaufgabe 5.2: ****/
+        if (data[l] < data[r])
+            swap(data, l, r);
+        int p1 = data[l];
+        int p2 = data[r];
+        int m1 = l + 1;
+        int m2 = r - 1;
+        int i = l + 1;
 
-        // Ersetzen Sie diese Kommentarzeile durch Ihren Code!
+        while (i <= m2) {
+            if (data[i] > p1) {
+                swap(data, i, m1);
+                m1++;
+            } else if (data[i] < p2) {
+                swap(data, i, m2);
+                m2--;
+                i--;
+            }
+            i++;
+        }
 
-        /**********************************************************/
-
-        return new int[] {
-                0,
-                0
-        };
+        m1--;
+        m2++;
+        swap(data, l, m1);
+        swap(data, r, m2);
+        return new int[] { m1, m2 };
     }
 
     // l,r inclusive -> sort data[l,r]
@@ -49,6 +78,16 @@ class B5A2 {
         // Ersetzen Sie diese Kommentarzeile durch Ihren Code!
 
         /**********************************************************/
+
+        if (l < r) {
+            int[] pivots = partition(data, l, r);
+            int m1 = pivots[0];
+            int m2 = pivots[1];
+            assert isPartitioned(data, l, r, m1, data[m1], m2, data[m2]);
+            qsort(data, l, m1 - 1);
+            qsort(data, m1 + 1, m2 - 1);
+            qsort(data, m2 + 1, r);
+        }
 
     }
 
@@ -70,11 +109,40 @@ class B5A2 {
             System.out.println(Arrays.toString(arr));
         }
 
-        /**********************************************************/
-        /**** Kurzaufgabe 5.2: ****/
+        // Laufzeitmessung starten
+        Instant start = Instant.now();
 
-        // Ersetzen Sie diese Kommentarzeile durch Ihren Code!
+        qsort(arr, 0, arr.length - 1);
 
-        /**********************************************************/
+        // Laufzeitmessung beenden
+        Instant finish = Instant.now();
+        long time = Duration.between(start, finish).toMillis();
+        System.out.println("Time: " + time + " ms");
+
+        // Nach dem Sortieren ausgeben, wenn die Länge weniger als 20 beträgt
+        if (arr.length < 20) {
+            System.out.println("After sorting:");
+            System.out.println(Arrays.toString(arr));
+        }
+
+        assert isSorted(arr) : "Array is not sorted!";
+    }
+
+    // utils Methoden
+
+    // Methode zum swapen von zwei Elementen in einem Array
+    private static void swap(int[] data, int i, int j) {
+        int temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+    }
+
+    // Methode zur Überprüfung, ob das Array absteigend sortiert ist
+    public static boolean isSorted(int[] data) {
+        for (int i = 1; i < data.length; i++) {
+            if (data[i - 1] < data[i])
+                return false;
+        }
+        return true;
     }
 }
